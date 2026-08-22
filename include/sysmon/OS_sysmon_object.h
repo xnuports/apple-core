52685d3af73aed00ad8e066d3e0c7b3418428ddd
somestupidgirl <sunnevanattsol@gmail.com>
2026-08-22 13:43:07 +0000

    include: add sysmon headers

---
+++ include/sysmon/OS_sysmon_object.h
@@ -0,0 +1,16 @@
+//
+//  OS_sysmon_object.h
+//  libsysmon
+//
+//  Reverse-engineered from macOS 26.5 dyld_shared_cache
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OS_sysmon_object <NSObject>
+@required
+@optional
+@end
+
+@interface OS_sysmon_object : OS_object <OS_sysmon_object>
+@end
